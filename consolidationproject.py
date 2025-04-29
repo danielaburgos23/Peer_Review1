4b96eee5dcfd7c318af134bbbb34485dc4d41ac1
Miriam Lahr <miriamlahr@Miriams-MacBook-Air.local>
2025-04-28 20:02:53 -0400

    testing..... creating cards

---
+++ consolidationproject.py
@@ -0,0 +1,7 @@
+import os
+import random
+
+def cards_deck():
+    face = ["A:1", "2:2", "3:3", "4:4", "5:5", "6:6", "7:7", "8:8", "9:9", "10:10", "J:11", "Q:12"]
+    face2 = ["Hearts", "Diamonds", "Spades", "Clubs"]
+    deck = []

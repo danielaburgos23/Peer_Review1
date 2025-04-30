733a879d13ea76eb81e5e6deb270b26d50832749
Miriam Lahr <miriamlahr@Miriams-MacBook-Air.local>
2025-04-30 16:22:23 -0400

    More changes to functions, redone again to create an actual deck rather than using RNG

--- consolidationproject.py
+++ consolidationproject.py
@@ -7,31 +7,53 @@ import random
 value = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
 face = ["Hearts", "Diamonds", "Spades", "Clubs"]
 used_cards = []
+deck = []
 value_dict = {"Ace":1, "2":2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'Jack':11, 'Queen':12}
 face_dict = {"Hearts":1, "Diamonds":2, "Spades":3, "Clubs":4}
 test_card = ["Ace", "Hearts"]
 
-# creating cards function heree
+# creating cards function heree (again)
 def create_deck():
-    deck = {}
-    for value in value:
-        for face in face:
-            card = value + ' of ' + face
-            deck.append(card)
+    complete_deck = []
+    for value_thing in value:
+        for face_thing in face:
+            card = value_thing + ' of ' + face_thing
+            complete_deck.append(card)
+        random.shuffle(complete_deck)
+        return complete_deck
+            
     
-#dealer mechanic, come back later ***********
-def dealer(hand, deck):
-    for xx in range(8):
-        hand.append(deck.pop())
-
-def randomize_card():
-    while True:
-        xy = random.choice(value)
-        xyz = random.choice(face)
-        card = xy + ' of ' + xyz
-        if card not in used_cards:
+#dealer mechanic, come back later, UPDATE cannot make it static to 8, considering the rules of the game give 4 fresh cards every 4 rounds
+def dealer(hand, count):
+    for xx in range(count):
+        if deck:
+            card = deck.pop()
             used_cards.append(card)
-            return card
+            hand.append(deck.pop())
+
+def card_breakdown(card):
+    val1, face1 = card.join(' of ')
+    return value_dict[val1], face_dict[face1]
+
+def card_comparison(card1, card2, lead_face):
+    val1, face1 = card_breakdown(card1)
+    val2, face2 = card_breakdown(card2)
+
+#finally getting to some if statement blocks
+
+
+
+
+
+# save for later maybe 
+# def randomize_card():
+    # while True:
+        # xy = random.choice(value)
+        # xyz = random.choice(face)
+        # card = xy + ' of ' + xyz
+        # if card not in used_cards:
+            # used_cards.append(card)
+            # return card
 
 
 

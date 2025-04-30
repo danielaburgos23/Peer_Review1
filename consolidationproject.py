cf260234e2412434ac05ef101433879c10283881
Miriam Lahr <miriamlahr@Miriams-MacBook-Air.local>
2025-04-30 17:08:19 -0400

    changed if block again to add tie condition, no bug fix yet (reminder)

--- consolidationproject.py
+++ consolidationproject.py
@@ -40,14 +40,22 @@ def card_comparison(card1, card2, lead_face):
     val2, face2 = card_breakdown(card2)
 
     if face1 == face2:
-        return "Player 1" if val1 > val2 else "Player 2"
+        if val1 > val2:
+            return "Player 1"
+        elif val2 > val1:
+            return "Player 2"
+        else:
+            return starting_player
     elif face1 == lead_face and face2 != lead_face:
         return "Player 1"
     elif face2 == lead_face and face1 != lead_face:
         return "Player 2"
-    else:
-        return "HA A TIE, starting player wins this round!"
-    #well as i wrote this i realized i need conditions for a tie so REDO
+
+
+        
+    
+    
+        
 
 
 
